fcda5a9c353d1d5f66cee4f58fe54652ef77f566
Sm9090 <sameer.m@xeverse.io>
2024-10-04 04:37:22 +0500

    feat: userModel and videoModel

---
+++ src/models/video.model.js
@@ -0,0 +1,41 @@
+import { Schema, model } from "mongoose";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, // third party Service
+      required: true,
+    },
+    thumbnail: {
+      type: String, // third party Service
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number, // third party Service
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Video = model("Video", videoSchema);
